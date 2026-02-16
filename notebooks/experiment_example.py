1d1c5d243493346bebc20b60d7c5ec8d4936325e
Alexander Norton <alexander.norton@climate-energy-college.org>
2026-02-16 16:27:56 +1100

    Updates to notebook examples
    More concise and clear examples on setting up an Experiment (in
    experiment_example.py) and running the model including plotting
    and saving results (model_run.py).

--- notebooks/experiment_example.py
+++ notebooks/experiment_example.py
@@ -1,3 +1,6 @@
+# %% [markdown]
+# ## 0. Load relevant modules
+
 # %%
 import pandas as pd
 from daesim2_analysis.experiment import Experiment
@@ -5,13 +8,65 @@ from daesim2_analysis.parameters import Parameters
 from daesim2_analysis.daesim_config import DAESIMConfig
 from daesim2_analysis.utils import load_df_forcing
 
+# %% [markdown]
+# ## 1. Load forcing data into a Pandas DataFrame
+
 # %%
-# 1. Load forcing data into a DataFrame
-# df_force = pd.read_csv("data/forcings1.csv", parse_dates=["Date"])
 df_forcing = load_df_forcing("../DAESIM_data/DAESim_forcing_data/DAESim_forcing_Milgadara_2018.csv")
 
 # %% [markdown]
-# 2. Create a Parameters object (e.g. from JSON or dict)
+# ## 2. DAESIM2 configuration file (e.g. from JSON or dict)
+
+# %% [markdown]
+# ### E.g. load from JSON file (best practice)
+
+# %%
+daesim_config = DAESIMConfig.from_json_dict("../daesim_configs/DAESIM1.json")  # or: DAESIMConfig.from_dict(your_config_dict)
+
+# %% [markdown]
+# ### E.g. define in-line
+
+# %%
+d_config = {
+    "plantgrowthphases.PlantGrowthPhases": {
+        "phases": ["germination","vegetative","spike","anthesis","grainfill","maturity"],
+        "gdd_requirements": [50,800,280,150,300,300],
+        "vd_requirements": [0,30,0,0,0,0],
+        "allocation_coeffs": [
+            [0.2,0.1,0.7,0.0,0.0],
+            [0.5,0.1,0.4,0.0,0.0],
+            [0.3,0.4,0.3,0.0,0.0],
+            [0.3,0.4,0.3,0.0,0.0],
+            [0.1,0.1,0.1,0.7,0.0],
+            [0.1,0.1,0.1,0.7,0.0]
+        ],
+        "turnover_rates": [
+            [0.001,0.001,0.001,0.0,0.0],
+            [0.01,0.002,0.008,0.0,0.0],
+            [0.01,0.002,0.008,0.0,0.0],
+            [0.01,0.002,0.008,0.0,0.0],
+            [0.033,0.016,0.033,0.0002,0.0],
+            [0.10,0.033,0.10,0.0002,0.0]
+        ]
+    },
+    "canopylayers.CanopyLayers": {
+        "nlevmlcan": 3
+    }
+}
+
+daesim_config2 = DAESIMConfig.from_dict(d_config)
+
+# %% [markdown]
+# ## 3. Create a `Parameters` object
+
+# %% [markdown]
+# ### E.g. load from JSON file (best practice)
+
+# %%
+parameters = Parameters.__from_file__("../parameters/Fast1.json")
+
+# %% [markdown]
+# ### E.g. define in-line
 
 # %%
 parameters = Parameters(
@@ -26,11 +81,16 @@ parameters = Parameters(
         phase           = [None, None, None, None, None, None, None, "vegetative", "grainfill", None, None, None, None]
 )
 
-# %%
-daesim_config = DAESIMConfig.from_json_dict("../daesim_configs/DAESIM1.json")  # or: DAESIMConfig.from_dict(your_config_dict)
+# %% [markdown]
+# ### E.g. ignore `Parameters` altogether (rely on default parameters and the configuration)
 
 # %%
-# # 4. Instantiate Experiment with DataFrame, Parameters, and DAESIMConfig
+parameters = Parameters.__from_file__("../parameters/paramsNone.json")
+
+# %% [markdown]
+# ## 4. Instantiate an `Experiment`
+
+# %%
 exp_programmatic = Experiment(
 	xsite="TestSite",
 	crop_type='Wheat',
@@ -46,3 +106,5 @@ exp_programmatic = Experiment(
 print(exp_programmatic.ForcingDataX.df.head())
 print(exp_programmatic.parameters.df.head())
 print(exp_programmatic.daesim_config.df.head())
+
+# %%
--- notebooks/model_run.py
+++ notebooks/model_run.py
@@ -36,23 +36,15 @@ from daesim2_analysis.forcing_data import ForcingData
 from daesim2_analysis.run import *
 
 # %%
-# # experiment = Experiment.from_cli() if not is_interactive() else Experiment()
-# experiment = Experiment(
-#     df_forcing="../DAESIM_data/DAESim_forcing_data/DAESim_forcing_Milgadara_2018.csv",
-#     parameters="../parameters/Fast1.json",
-#     daesim_config="../daesim_configs/DAESIM1.json",
-#     dir_results="../results/",
-# )
-
+# experiment = Experiment.from_cli() if not is_interactive() else Experiment()
 experiment = Experiment(
-    df_forcing="../DAESIM_data/DAESim_forcing_data/DAESim_forcing_Harden_2008_SC4_v1.csv",
+    df_forcing="../DAESIM_data/DAESim_forcing_data/DAESim_forcing_Milgadara_2018.csv",
     parameters="../parameters/Fast1.json",
     daesim_config="../daesim_configs/DAESIM1.json",
     dir_results="../results/",
-    df_forcing_type='3',
-    sowing_dates=[Timestamp(year=2008,month=5,day=14)],
-    harvest_dates=[Timestamp(year=2008,month=11,day=17)],
-    xsite="Harden_2008",
+    sowing_dates=[Timestamp(year=2018,month=5,day=15)],
+    harvest_dates=[Timestamp(year=2018,month=12,day=31)],
+    xsite="Milgadara_2018",
 )
 
 # %%
@@ -105,98 +97,130 @@ d_fd_mapping = {
     'Climate_year_f': 'forcing 12'}
 
 # %%
+## Create datetime array from doy and year outputs
+xyear = np.array(model_output[d_fd_mapping['Climate_year_f']], dtype=int)
+xdoy = model_output[d_fd_mapping['Climate_doy_f']]
+model_output['Date'] = pd.to_datetime(xyear.astype(str), format='%Y') + pd.to_timedelta(xdoy - 1, unit='D')
+
+# %%
 fig, axes = plt.subplots(4,1,figsize=(8,8),sharex=True)
 
-axes[0].plot(model_output[d_fd_mapping['Climate_doy_f']], model_output[d_fd_mapping['Climate_solRadswskyb_f']] + model_output[d_fd_mapping['Climate_solRadswskyd_f']], c='0.4', label="Global")
-axes[0].plot(model_output[d_fd_mapping['Climate_doy_f']], model_output[d_fd_mapping['Climate_solRadswskyb_f']], c='goldenrod', alpha=0.5, label="Direct")
-axes[0].plot(model_output[d_fd_mapping['Climate_doy_f']], model_output[d_fd_mapping['Climate_solRadswskyd_f']], c='C0', alpha=0.5, label="Diffuse")
+axes[0].plot(model_output['Date'], model_output[d_fd_mapping['Climate_solRadswskyb_f']] + model_output[d_fd_mapping['Climate_solRadswskyd_f']], c='0.4', label="Global")
+axes[0].plot(model_output['Date'], model_output[d_fd_mapping['Climate_solRadswskyb_f']], c='goldenrod', alpha=0.5, label="Direct")
+axes[0].plot(model_output['Date'], model_output[d_fd_mapping['Climate_solRadswskyd_f']], c='C0', alpha=0.5, label="Diffuse")
 axes[0].set_ylabel("Solar radiation\n"+r"($\rm W \; m^{-2}$)")
 axes[0].legend(loc=1,handlelength=0.75)
-# axes[0].tick_params(axis='x', labelrotation=45)
 axes[0].annotate("Solar radiation", (0.02,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
 axes[0].set_ylim([0,400])
 
-axes[1].plot(model_output[d_fd_mapping['Climate_doy_f']], model_output[d_fd_mapping['Climate_airTempCMin_f']], c="lightsteelblue", label="Min")
-axes[1].plot(model_output[d_fd_mapping['Climate_doy_f']], model_output[d_fd_mapping['Climate_airTempCMax_f']], c="indianred", label="Max")
+axes[1].plot(model_output['Date'], model_output[d_fd_mapping['Climate_airTempCMin_f']], c="lightsteelblue", label="Min")
+axes[1].plot(model_output['Date'], model_output[d_fd_mapping['Climate_airTempCMax_f']], c="indianred", label="Max")
 leafTempC = experiment.PlantX.Site.compute_skin_temp((model_output[d_fd_mapping['Climate_airTempCMin_f']] + model_output[d_fd_mapping['Climate_airTempCMax_f']])/2, model_output[d_fd_mapping['Climate_solRadswskyb_f']] + model_output[d_fd_mapping['Climate_solRadswskyd_f']])
-axes[1].plot(model_output[d_fd_mapping['Climate_doy_f']], leafTempC, c="darkgreen", label="Leaf")
-axes[1].plot(model_output[d_fd_mapping['Climate_doy_f']], (model_output[d_fd_mapping['Climate_airTempCMin_f']] + model_output[d_fd_mapping['Climate_airTempCMax_f']])/2, c="0.5", label="Air")
+axes[1].plot(model_output['Date'], leafTempC, c="darkgreen", label="Leaf")
+axes[1].plot(model_output['Date'], (model_output[d_fd_mapping['Climate_airTempCMin_f']] + model_output[d_fd_mapping['Climate_airTempCMax_f']])/2, c="0.5", label="Air")
 axes[1].set_ylabel("Air Temperature\n"+r"($\rm ^{\circ}C$)")
-# axes[1].tick_params(axis='x', labelrotation=45)
 axes[1].legend(loc=1,handlelength=0.75)
 axes[1].annotate("Air temperature", (0.02,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
 axes[1].set_ylim([-5,45])
 
-axes[2].plot(model_output[d_fd_mapping['Climate_doy_f']], model_output[d_fd_mapping['Climate_airRH_f']], c="0.4")
+axes[2].plot(model_output['Date'], model_output[d_fd_mapping['Climate_airRH_f']], c="0.4")
 axes[2].set_ylabel("Relative humidity\n"+r"(%)")
-# axes[2].tick_params(axis='x', labelrotation=45)
 axes[2].annotate("Relative humidity", (0.02,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
 
 xcolors = np.linspace(0.9,0.1,experiment.PlantX.PlantCH2O.SoilLayers.nlevmlsoil).astype(str)
 for iz in range(experiment.PlantX.PlantCH2O.SoilLayers.nlevmlsoil):
     zlayer_forcing_label = d_fd_mapping['Climate_soilTheta_z_f'] + f' z{iz}'
-    axes[3].plot(model_output[d_fd_mapping['Climate_doy_f']], 100*model_output[zlayer_forcing_label], c=xcolors[iz])
+    axes[3].plot(model_output['Date'], 100*model_output[zlayer_forcing_label], c=xcolors[iz])
 axes[3].set_ylabel("Soil moisture\n"+r"(%)")
 # axes[3].tick_params(axis='x', labelrotation=45)
 axes[3].annotate("Soil moisture", (0.02,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
-# axes[3].set_ylim([20,50])
-axes[3].set_xlabel("Time (day of year)")
 
 if "Precipitation" in experiment.df_forcing.columns:
     ax2 = axes[3].twinx()
     i0, i1 = experiment.ForcingDataX.time_axis[0]-1, experiment.ForcingDataX.time_axis[-1]
-    ax2.bar(model_output[d_fd_mapping['Climate_doy_f']], experiment.df_forcing["Precipitation"].values[i0:i1], color="0.4")
+    ax2.bar(model_output['Date'], experiment.df_forcing["Precipitation"].values[i0:i1], color="0.4")
     ax2.set_ylabel("Daily Precipitation\n(mm)")
     axes[3].annotate("Precipitation", (0.98,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='right', fontsize=12)
 
-axes[0].set_xlim([experiment.PlantX.Management.sowingDays[0],model_output[d_fd_mapping['Climate_doy_f']][-1]])
+# axes[0].set_xlim([experiment.PlantX.Management.sowingDays[0],model_output[d_fd_mapping['Climate_doy_f']][-1]])
 
 plt.tight_layout()
 
 # %%
 fig, axes = plt.subplots(5,1,figsize=(8,10),sharex=True)
 
-axes[0].plot(model_output['t'], model_output["LAI"])
+axes[0].plot(model_output['Date'], model_output["LAI"])
 axes[0].set_ylabel("LAI\n"+r"($\rm m^2 \; m^{-2}$)")
 axes[0].tick_params(axis='x', labelrotation=45)
 axes[0].annotate("Leaf area index", (0.01,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
-axes[0].set_ylim([0,6.5])
+# axes[0].set_ylim([0,6.5])
 
-axes[1].plot(model_output["t"], model_output["GPP"])
+axes[1].plot(model_output['Date'], model_output["GPP"])
 axes[1].set_ylabel("GPP\n"+r"($\rm g C \; m^{-2} \; d^{-1}$)")
 axes[1].tick_params(axis='x', labelrotation=45)
 axes[1].annotate("Photosynthesis", (0.01,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
-axes[1].set_ylim([0,30])
+# axes[1].set_ylim([0,30])
 
-axes[2].plot(model_output["t"], model_output["E_mmd"])
+axes[2].plot(model_output['Date'], model_output["E_mmd"])
 axes[2].set_ylabel(r"$\rm E$"+"\n"+r"($\rm mm \; d^{-1}$)")
 axes[2].tick_params(axis='x', labelrotation=45)
 axes[2].annotate("Transpiration Rate", (0.01,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
-axes[2].set_ylim([0,6])
+# axes[2].set_ylim([0,6])
 
-axes[3].plot(model_output["t"], model_output["Bio_time"])
+axes[3].plot(model_output['Date'], model_output["Bio_time"])
 axes[3].set_ylabel("Thermal Time\n"+r"($\rm ^{\circ}$C d)")
 axes[3].annotate("Growing Degree Days", (0.01,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
 
+
 alp = 0.6
-axes[4].plot(model_output["t"], model_output["Cleaf"]+model_output["Croot"]+model_output["Cstem"]+model_output["Cseed"],c='k',label="Plant", alpha=alp)
-axes[4].plot(model_output["t"], model_output["Cleaf"],label="Leaf", alpha=alp)
-axes[4].plot(model_output["t"], model_output["Cstem"],label="Stem", alpha=alp)
-axes[4].plot(model_output["t"], model_output["Croot"],label="Root", alpha=alp)
-axes[4].plot(model_output["t"], model_output["Cseed"],label="Seed", alpha=alp)
+axes[4].plot(model_output['Date'], model_output["Cleaf"]+model_output["Croot"]+model_output["Cstem"]+model_output["Cseed"],c='k',label="Plant", alpha=alp)
+axes[4].plot(model_output['Date'], model_output["Cleaf"],label="Leaf", alpha=alp)
+axes[4].plot(model_output['Date'], model_output["Cstem"],label="Stem", alpha=alp)
+axes[4].plot(model_output['Date'], model_output["Croot"],label="Root", alpha=alp)
+axes[4].plot(model_output['Date'], model_output["Cseed"],label="Seed", alpha=alp)
 axes[4].set_ylabel("Carbon Pool Size\n"+r"(g C $\rm m^{-2}$)")
-axes[4].set_xlabel("Time (day of year)")
-axes[4].legend(loc=3,fontsize=9,handlelength=0.8)
+axes[4].legend(fontsize=9,handlelength=0.8)
 
-# Time indexing for model output data, to determine outputs at specific times in the growing season
-itax_sowing, itax_mature, itax_harvest, itax_phase_transitions = experiment.PlantX.Site.time_index_growing_season(experiment.ForcingDataX.time_index, model_output['idevphase_numeric'], experiment.PlantX.Management, experiment.PlantX.PlantDev)
-harvest_index_maturity = model_output["Cseed"][itax_harvest] / (model_output["Cleaf"][itax_mature]+model_output["Croot"][itax_mature]+model_output["Cstem"][itax_mature])
-yield_from_seed_Cpool = model_output["Cseed"][itax_harvest]/100 * (1/experiment.PlantX.PlantCH2O.f_C)   ## convert gC m-2 to t dry biomass ha-1
-axes[4].annotate("Yield = %1.2f t/ha" % (yield_from_seed_Cpool), (0.01,0.93), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
-axes[4].annotate("Harvest index = %1.2f" % (harvest_index_maturity), (0.01,0.81), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=12)
 
-axes[0].set_xlim([experiment.PlantX.Management.sowingDays[0],model_output[d_fd_mapping['Climate_doy_f']][-1]])
+xlimmin, xlimmax = experiment.ForcingDataX.sowing_dates[0], experiment.ForcingDataX.harvest_dates[-1]
+
+for iseason in range(len(experiment.ForcingDataX.sowing_dates)):
+    # Time indexing for model output data, to determine outputs at specific times in the growing season
+    itax_sowing, itax_mature, itax_harvest, itax_phase_transitions = experiment.PlantX.Site.time_index_growing_season(experiment.ForcingDataX.time_index, model_output['idevphase_numeric'], experiment.PlantX.Management, experiment.PlantX.PlantDev, iseason=iseason)
+    harvest_index_maturity = model_output["Cseed"][itax_harvest] / (model_output["Cleaf"][itax_mature]+model_output["Croot"][itax_mature]+model_output["Cstem"][itax_mature])
+    yield_from_seed_Cpool = model_output["Cseed"][itax_harvest]/100 * (1/experiment.PlantX.PlantCH2O.f_C)   ## convert gC m-2 to t dry biomass ha-1
+    xyear = int(model_output[d_fd_mapping['Climate_year_f']][itax_harvest])
+    axes[4].annotate(f"{xyear} Yield = %1.2f t/ha" % (yield_from_seed_Cpool), (0.01,0.96-0.1*iseason), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=9)
+    # axes[4].annotate(f"{xyear} Harvest index = %1.2f" % (harvest_index_maturity), (0.01,0.5-0.1*iseason), xycoords='axes fraction', verticalalignment='top', horizontalalignment='left', fontsize=9)
+
+    # Add annotations for developmental phases
+    itax_sowing, itax_mature, itax_harvest, itax_phase_transitions = experiment.PlantX.Site.time_index_growing_season(experiment.ForcingDataX.time_index, model_output['idevphase_numeric'], experiment.PlantX.Management, experiment.PlantX.PlantDev, iseason=iseason)
+    # for ax in axes:
+    ax = axes[3]
+    ylimmin, ylimmax = 0, ax.get_ylim()[1]
+    for itime in itax_phase_transitions:
+        ax.vlines(x=model_output['Date'][itime], ymin=ylimmin, ymax=ylimmax, color='0.5',linestyle="--")
+        text_x = model_output['Date'][itime] + pd.Timedelta(days=1)
+        text_y = 0.5 * ylimmax
+        if (text_x < xlimmin) or (text_x > xlimmax):
+            continue
+        elif ~np.isnan(model_output['idevphase_numeric'][itime]):
+            idevphase = int(model_output['idevphase'][itime])
+            phase = experiment.PlantX.PlantDev.phases[idevphase]
+            ax.text(text_x, text_y, phase, horizontalalignment='left', verticalalignment='center',
+                    fontsize=8, alpha=0.7, rotation=90)
+        elif np.isnan(model_output['idevphase_numeric'][itime]):
+            phase = "mature"
+            ax.text(text_x, text_y, phase, horizontalalignment='left', verticalalignment='center',
+                    fontsize=8, alpha=0.7, rotation=90)
+    ax.set_ylim([ylimmin, ylimmax])
 
 plt.tight_layout()
+
+# %%
+
+# %%
+
+# %%
 
 # %%
